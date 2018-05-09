947547d0add4c3afa23922a7e0cc5bc36f4e3c0d
Pig Fang <g-plane@hotmail.com>
2018-05-09 15:28:42 +0800

    goodbye hexo

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-cd ./public
-git init
-git config --local user.name "gplane"
-git config --local user.email "g-plane@hotmail.com"
-git add -A
-git commit -m "Blog updated at `date +"%Y-%m-%d %H:%M:%S"`"
-git push https://gplane:$coding_token@git.coding.net/gplane/gplane.coding.me.git master --force
