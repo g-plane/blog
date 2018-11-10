d7a572ac374eeb8e01f1d4734adf4db3794b113e
Pig Fang <g-plane@hotmail.com>
2018-11-10 18:56:25 +0800

    add scaffold

---
+++ scripts/scaffold.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+const title = process.argv[2]
+
+const template = `---
+title: ${title}
+categories:
+  - TO BE FILLED
+tags:
+  - TO BE FILLED
+created_at: ${now()}
+view: post
+layout: post
+author: gplane
+lang: en
+---
+`
+
+fs.writeFileSync(`./website/posts/${title}.md`, template)
+
+function now() {
+  const d = new Date()
+  return (
+    d.getFullYear() +
+    '-' +
+    d.getMonth() +
+    '-' +
+    d.getDate() +
+    ' ' +
+    d.getHours() +
+    ':' +
+    d.getMinutes() +
+    ':' +
+    d.getSeconds()
+  )
+}
