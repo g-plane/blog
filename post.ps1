6e69355a2a4d016c1b71ffd0d81bc98e5885a1b1
Pig Fang <g-plane@hotmail.com>
2019-06-17 12:00:27 +0800

    add vscode task

--- post.ps1
+++ post.ps1
@@ -1,3 +1,4 @@
 $slug = $args[0]
+$title = $args[1]
 $date = Get-Date -UFormat "%Y-%m-%d %T"
-Set-Content -Path "./website/posts/$slug.md" -Value "---`ntitle: `ndate: $date`ntags:`n---`n`n"
+Set-Content -Path "./website/posts/$slug.md" -Value "---`ntitle: $title`ndate: $date`ntags:`n---`n`n"
